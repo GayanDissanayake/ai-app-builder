413f6700ca5c9dd5ea803b806cf4164f995bb93f
GayanDissanayake <gayandissanayake11@gmail.com>
2025-09-17 17:13:46 +1200

    Add CI, test & mongooese packages

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5050;
 
+// const mongoose = require('mongoose');
+// mongoose.connect('your_connection_string')
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
