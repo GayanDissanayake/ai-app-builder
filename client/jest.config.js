e4c8bf366d71f26df04773a37f3484e6807ba9cf
GayanDissanayake <gayandissanayake11@gmail.com>
2025-09-19 15:19:42 +1200

    Update sercer deployment error

--- client/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testEnvironment: "jsdom",
-  testMatch: ["**/*.test.ts?(x)"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
